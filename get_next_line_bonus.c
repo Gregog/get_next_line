3438a1848d6354821a88c336d523fad9590e7d69
Ilia Petrov <idzaoshang@yandex.ru>
2020-05-13 21:07:50 +0300

    initial commit

---
+++ get_next_line_bonus.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rvernius <rvernius@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/09 15:23:50 by rvernius          #+#    #+#             */
+/*   Updated: 2020/05/12 21:48:56 by rvernius         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+static int		ft_make_new_line(char **fd_arr, char **line, char *p_new_line)
+{
+	char *temp;
+
+	*p_new_line++ = '\0';
+	*line = ft_strdup(*fd_arr);
+	if (!line)
+		return (-1);
+	if (*p_new_line == '\0')
+	{
+		free(*fd_arr);
+		*fd_arr = NULL;
+		return (1);
+	}
+	temp = ft_strdup(p_new_line);
+	free(*fd_arr);
+	*fd_arr = temp;
+	return (1);
+}
+
+static int		ft_check_remainder(char **fd_arr, char **line, int bwr)
+{
+	char *p_new_line;
+
+	if (bwr < 0)
+		return (-1);
+	if (*fd_arr && (p_new_line = ft_strchr(*fd_arr)))
+		return (ft_make_new_line(fd_arr, line, p_new_line));
+	if (*fd_arr)
+	{
+		*line = *fd_arr;
+		*fd_arr = NULL;
+		return (0);
+	}
+	*line = ft_strdup("");
+	return (0);
+}
+
+int				get_next_line(int fd, char **line)
+{
+	static char	*fd_arr[FD];
+	char		buff[BUFFER_SIZE + 1];
+	int			bwr;
+	char		*p_new_line;
+
+	if ((!line) || (fd < 0) || (read(fd, buff, 0) < 0))
+		return (-1);
+	while ((bwr = read(fd, buff, BUFFER_SIZE)) > 0)
+	{
+		buff[bwr] = '\0';
+		fd_arr[fd] = ft_strjoin(fd_arr[fd], buff);
+		if (!fd_arr[fd])
+			return (-1);
+		if ((p_new_line = ft_strchr(fd_arr[fd])))
+			return (ft_make_new_line(&fd_arr[fd], line, p_new_line));
+	}
+	return (ft_check_remainder(&fd_arr[fd], line, bwr));
+}
---
+++ get_next_line_bonus.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rvernius <rvernius@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/09 16:56:47 by rvernius          #+#    #+#             */
+/*   Updated: 2020/05/12 16:51:05 by rvernius         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+# define FD 15000
+
+int				get_next_line(int fd, char **line);
+char			*ft_strdup(char *str);
+char			*ft_strjoin(char *s1, char *s2);
+size_t			ft_strlen(const char *s);
+char			*ft_strchr(char *s);
+#endif
